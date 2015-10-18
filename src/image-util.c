8197671bf1f81e7933ff0029574179cc9ea77ef6
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2015-10-18 10:04:05 +0000

    toBlob

--- src/image-util.c
+++ src/image-util.c
@@ -233,3 +233,114 @@ char *image_to_data_url(char *type, float encoder_options, size_t *data_base64_a
 	
 	return data_base64;
 }
+
+char *image_to_blob(char *type, float encoder_options, size_t *data_amount)
+{
+	unsigned int x = 0;
+	unsigned int y = 0;
+	RGBQUAD color = { 0, 0, 0, 0 };
+	FIMEMORY *memory_stream = NULL;
+	FIBITMAP *image = FreeImage_Allocate(egl_get_width(), egl_get_height(), 32, 0xFF000000, 0x00FF0000, 0x0000FF00);
+	unsigned char *data = malloc(egl_get_width() * egl_get_height() * 4);
+	unsigned char *data_copy = NULL;
+	FREE_IMAGE_FORMAT save_format = FIF_PNG;
+	int save_flags = 0;
+	char *save_prefix = "";
+	
+	if(data == NULL || !image)
+	{
+		eprintf("Failed to create blob.\n");
+		
+		*data_amount = 0;
+		
+		return NULL;
+	}
+	
+	vgReadPixels(data, (egl_get_width() * 4), VG_sRGBX_8888, 0, 0, egl_get_width(), egl_get_height());
+	
+	for(y = 0; y < (unsigned int)egl_get_height(); y++)
+	{
+		for(x = 0; x < (unsigned int)egl_get_width(); x++)
+		{
+			color.rgbRed = data[(y * egl_get_width() + x) * 4 + 3];
+			color.rgbGreen = data[(y * egl_get_width() + x) * 4 + 2];
+			color.rgbBlue = data[(y * egl_get_width() + x) * 4 + 1];
+			color.rgbReserved = data[(y * egl_get_width() + x) * 4 + 0];
+			
+			FreeImage_SetPixelColor(image, x, y, &color);
+		}
+	}
+	
+	free(data);
+	
+	if(!strcmp(type, "image/png"))
+	{
+		save_format = FIF_PNG;
+		save_flags = PNG_DEFAULT;
+		save_prefix = "data:image/png;base64,";
+	}
+	else if(!strcmp(type, "image/jpeg"))
+	{
+		save_format = FIF_JPEG;
+		if(encoder_options <= 1 && encoder_options >= 0)
+		{
+			save_flags = encoder_options * 100;
+		}
+		else
+		{
+			save_flags = JPEG_DEFAULT;
+		}
+		save_prefix = "data:image/jpeg;base64,";
+	}
+	else
+	{
+		save_format = FIF_PNG;
+		save_flags = PNG_DEFAULT;
+		save_prefix = "data:image/png;base64,";
+	}
+	
+	memory_stream = FreeImage_OpenMemory(NULL, 0);
+	
+	if(!FreeImage_SaveToMemory(save_format, image, memory_stream, save_flags))
+	{
+		eprintf("Failed to save blob to memory.\n");
+		
+		FreeImage_Unload(image);
+		FreeImage_CloseMemory(memory_stream);
+		
+		*data_amount = 0;
+		
+		return NULL;
+	}
+	
+	FreeImage_Unload(image);
+	
+	if(!FreeImage_AcquireMemory(memory_stream, (BYTE **)&data, data_amount))
+	{
+		eprintf("Failed to acquire blob.\n");
+		
+		FreeImage_CloseMemory(memory_stream);
+		
+		*data_amount = 0;
+		
+		return NULL;
+	}
+	
+	data_copy = malloc(*data_amount);
+	if(data_copy == NULL)
+	{
+		eprintf("Failed to acquire blob.\n");
+		
+		FreeImage_CloseMemory(memory_stream);
+		
+		*data_amount = 0;
+		
+		return NULL;
+	}
+	
+	memcpy(data_copy, data, *data_amount);
+	
+	FreeImage_CloseMemory(memory_stream);
+	
+	return data_copy;
+}
--- src/image-util.h
+++ src/image-util.h
@@ -1,7 +1,25 @@
+/**
+ * Copyright (C) 2015 NIPE-SYSTEMS
+ * Copyright (C) 2015 Hauke Oldsen
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __IMAGE_UTIL_H__
 #define __IMAGE_UTIL_H__
 
-#include "include-openvg.h"
+#include <VG/openvg.h>
 #include <FreeImage.h>
 
 typedef struct image_t {
@@ -15,5 +33,6 @@ image_t* image_create(FIBITMAP *bitmap);
 void image_cleanup(image_t *image);
 void image_free_bitmap(FIBITMAP *bitmap);
 char *image_to_data_url(char *type, float encoder_options, size_t *data_base64_amount);
+char *image_to_blob(char *type, float encoder_options, size_t *data_amount);
 
-#endif
+#endif /* __IMAGE_UTIL_H__ */
